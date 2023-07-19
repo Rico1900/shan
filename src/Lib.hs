0d9b53780384c9550e814c4453706437f4ad65f7
Rico <lull.scholar@gmail.com>
2023-07-19 15:29:04 +0800

    feat: optimize command line arguments parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,20 +9,28 @@ import Task.Shan (runExperiment1, runExperiment2, runSingle1, parallelRunExperim
 entry :: IO ()
 entry = do
   args <- getArgs
-  if length args == 1
+  if length args >= 2
     then case head args of
+      "parallel" -> parallelExperiment (drop 1 args)
+      _ -> sequenceExperiment args
+  else if length args == 1
+    then sequenceExperiment args
+  else error "invalid arguments"
+
+parallelExperiment :: [String] -> IO ()
+parallelExperiment args = do
+  case head args of
+      "experiment1" -> withArgs (drop 1 args) parallelRunExperiment1
+      "experiment2" -> withArgs (drop 1 args) parallelRunExperiment2
+      "single1" -> withArgs (drop 1 args) parallelRunSingle1
+      "single2" -> withArgs (drop 1 args) parallelRunSingle2
+      _ -> error "invalid arguments"
+
+sequenceExperiment :: [String] -> IO ()
+sequenceExperiment args = do
+  case head args of
       "experiment1" -> withArgs (drop 1 args) runExperiment1
       "experiment2" -> withArgs (drop 1 args) runExperiment2
       "single1" -> withArgs (drop 1 args) runSingle1
       "single2" -> withArgs (drop 1 args) runSingle2
       _ -> error "invalid arguments"
-  else if length args == 2
-    then case head args of
-      "parallel" -> case args !! 1 of
-        "experiment1" -> withArgs (drop 2 args) parallelRunExperiment1
-        "experiment2" -> withArgs (drop 2 args) parallelRunExperiment2
-        "single1" -> withArgs (drop 2 args) parallelRunSingle1
-        "single2" -> withArgs (drop 2 args) parallelRunSingle2
-        _ -> error "invalid arguments"
-      _ -> error "invalid arguments"
-  else error "invalid arguments"
